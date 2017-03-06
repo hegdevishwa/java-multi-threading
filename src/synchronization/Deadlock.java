f8359eb959eb89e3c40e16961ae8f4502b7d79b8
Vishwamitra Hegde <vishwamitra.hegde@gmail.com>
2017-03-06 11:30:53 +0530

    packaged the classes

--- src/synchronization/Deadlock.java
+++ src/synchronization/Deadlock.java
@@ -2,59 +2,36 @@ package synchronization;
 
 public class Deadlock {
 
-	public static void main(String[] args) {
+	public static void main(String[] rgs) {
+		String s1 = "abc";
+		String s2 = "def";
 
-		StringBuffer sb1 = new StringBuffer("abc");
-		StringBuffer sb2 = new StringBuffer("def");
+		Runnable r1 = () -> {
+			synchronized (s1) {
 
-		System.out.println("Before thread start");
-		Runnable runnable1 = () -> {
-			for (int i = 0; i < 100; i++) {
+				System.out.println("T1 Synchronized s1");
 
-				synchronized (sb1) {
-					System.out.println(Thread.currentThread().getName() + " has lock on sb1?" + Thread.holdsLock(sb1));
-					System.out.println(sb1.toString());
-					synchronized (sb2) {
-						System.out.println(
-								Thread.currentThread().getName() + " has lock on sb2?" + Thread.holdsLock(sb2));
-						System.out.println(sb2.toString());
-					}
+				synchronized (s2) {
+					System.out.println("T1 Synchronized s1");
 				}
-
 			}
-
 		};
 
-		Runnable runnable2 = () -> {
-			for (int i = 0; i < 100; i++) {
+		Runnable r2 = () -> {
+			synchronized (s2) {
 
-				// if in the below code sb2 is synchronized first it'll go into
-				// deadlock
-				synchronized (sb1) {
-					System.out.println(Thread.currentThread().getName() + " has lock on sb1?" + Thread.holdsLock(sb1));
-					System.out.println(sb1.toString());
-					synchronized (sb2) {
-						System.out.println(
-								Thread.currentThread().getName() + " has lock on sb2?" + Thread.holdsLock(sb2));
-						System.out.println(sb2.toString());
-					}
+				System.out.println("T2 Synchronized s2");
+
+				synchronized (s1) {
+					System.out.println("T2 Synchronized s1");
 				}
-
 			}
-
 		};
 
-		// We always need thread instances to run the thread. its a thread of
-		// execution
-		Thread t1 = new Thread(runnable1);
-		Thread t2 = new Thread(runnable2);
-		t1.setName("t1");
-		t2.setName("t2");
+		Thread t1 = new Thread(r1);
+		Thread t2 = new Thread(r2);
 		t1.start();
-
 		t2.start();
-
-		System.out.println("After thread start");
 
 	}
 
